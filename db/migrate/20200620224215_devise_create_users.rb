350dfe9abbffa42d00a5c8ec4781554bf971b864
thejames <james@zambon.us>
2020-06-24 20:41:49 -0400

    Update 20200620224215_devise_create_users.rb
    fixed username error

--- db/migrate/20200620224215_devise_create_users.rb
+++ db/migrate/20200620224215_devise_create_users.rb
@@ -37,9 +37,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.timestamps null: false
     end
 
-    add_index :username,  :email,                unique: true
-    add_index :users,     :email,                unique: true
-    add_index :users,     :reset_password_token, unique: true
+    add_index :users,  :username,             unique: true
+    add_index :users,  :email,                unique: true
+    add_index :users,  :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
