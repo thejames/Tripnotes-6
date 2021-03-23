5ecb4017214f153c424a63865d06384092e2a88f
theJames <james@zambon.us>
2021-03-22 20:00:31 -0400

    Update channel.rb

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -3,18 +3,18 @@ module ApplicationCable
     identified_by :current_user
 
     def connect
-      self.current_user = find_verified_user
+      self.current_user = current_user
     end
 
     private
 
-    def find_verified_user
-      # rubocop:disable Lint/AssignmentInCondition
-      if verified_user = User.find_by(id: cookies.signed['user.id'])
-        verified_user
-      else
-        reject_unauthorized_connection
-      end
-    end
+    # def find_verified_user
+    #   # rubocop:disable Lint/AssignmentInCondition
+    #   if verified_user = User.find_by(id: cookies.signed['user.id'])
+    #     verified_user
+    #   else
+    #     reject_unauthorized_connection
+    #   end
+    # end
   end
 end
