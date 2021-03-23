e4a7d32f3fba8b54428dd21914b001ea438c521d
theJames <james@zambon.us>
2021-03-23 00:58:06 -0400

    updates

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,10 +1,10 @@
 module ApplicationCable
-  # class Channel < ActionCable::Channel::Base
+  class Channel < ActionCable::Channel::Base
   #   identified_by :current_user
 
   #   def connect
   #     self.current_user = current_user
-  #   end
+    end
 
     private
 
