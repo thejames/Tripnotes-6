2e4c45700788bb013c3abbd533e50193f73e6759
theJames <james@zambon.us>
2021-03-24 13:12:56 -0400

    fixing actioncable click

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,19 +1,4 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
-  #   identified_by :current_user
-
-  #   def connect
-  #     self.current_user = current_user
-    end
-
-    private
-
-    # def find_verified_user
-    #   # rubocop:disable Lint/AssignmentInCondition
-    #   if verified_user = User.find_by(id: cookies.signed['user.id'])
-    #     verified_user
-    #   else
-    #     reject_unauthorized_connection
-    #   end
-    # end
+  end
 end
